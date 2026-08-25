da90089453145876328f79250c95f7a00858b138
Alícia Stefany <64162281+aliciastefany@users.noreply.github.com>
2026-08-25 14:12:06 +0000

    login update

---
+++ loginFirebase3bga/screens/Login.js
@@ -0,0 +1,30 @@
+import {View, Text, TextInput, Button, Alert} from 'react-native'
+import {useState} from 'react'
+
+export default function Login(){
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    return(
+        <View>
+            <Text> Login</Text>
+            <TextInput
+                placeholder='E-mail'
+                value={email}
+                onChangeText={setEmail}
+                keyboardType='email-address'
+                autoCapitalize='none'
+            />
+            <TextInput
+                placeholder='Senha'
+                value={senha}
+                onChangeText={setSenha}
+                secureTextEntry
+            />
+            <Button
+                title='Login'
+                onPress={()=>alert('Login em construção')}
+            />
+        </View>
+    )
+}
